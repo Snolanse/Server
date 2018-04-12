2eb107fae3303334b8a19b16da6268fd4e45a920
Tjore <35220757+Tjore@users.noreply.github.com>
2018-04-12 13:58:25 +0200

    Plot
    Fikset plotting med riktig intervall

--- lanseside/manage.py
+++ lanseside/manage.py
@@ -10,7 +10,7 @@ import yrWbPlot
 def yrgraf():
     print('graf er i gang')
     yrWbPlot.yrplot()
-    schedule.every(1).minutes.do(yrWbPlot.yrplot) #denne skal byttes til time
+    schedule.every(60).minutes.do(yrWbPlot.yrplot) #denne skal byttes til time
     while True:
         schedule.run_pending()
         time.sleep(10)
