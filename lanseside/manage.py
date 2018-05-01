98d8f32d13846d8322d5907529b1fbddd0f3c388
Tjore <35220757+Tjore@users.noreply.github.com>
2018-05-01 09:27:23 +0200

    øæå

--- lanseside/manage.py
+++ lanseside/manage.py
@@ -37,7 +37,7 @@ def planlagtArbeid(): #denne threaden skal ta seg av alle tidsbestemte oppgaver
     #time.sleep(3600)
     #yrgraf()
 
-if os.environ.get('RUN_MAIN') != 'true':        #sjekker hvilken 'thread' man kjorer i for å unngaa dobbelkjoring
+if os.environ.get('RUN_MAIN') != 'true':        #sjekker hvilken 'thread' man kjorer i for aa unngaa dobbelkjoring
     t = threading.Thread(target=planlagtArbeid, daemon=True)
     t.start()
 
