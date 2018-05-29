244058a5595947d91ec4d93cae8c40121386167a
Tjore <35220757+Tjore@users.noreply.github.com>
2018-05-29 10:55:50 +0200

    verdata admin
    la til tilgang på verdata databasen på /admin

--- lanseside/startside/admin.py
+++ lanseside/startside/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from startside.models import Lanse,Lansetype,langtidslagring
+from startside.models import Lanse,Lansetype,langtidslagring,Verdata
 
 # Register your models here.
 admin.site.register(Lanse)      #registrerer databasene slik at man faar tilgang paa admin-siden
 admin.site.register(Lansetype)
 admin.site.register(langtidslagring)
+admin.site.register(Verdata)
